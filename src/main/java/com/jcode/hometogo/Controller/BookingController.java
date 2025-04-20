48f831d2cb3286554eb203d667ffe4f9ea166030
Lily Griggs <lilygriggs@Mac.attlocal.net>
2025-04-20 19:04:22 -0400

    Booking form and MyBOokings

--- src/main/java/com/jcode/hometogo/Controller/BookingController.java
+++ src/main/java/com/jcode/hometogo/Controller/BookingController.java
@@ -5,6 +5,8 @@ import com.jcode.hometogo.Service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,16 @@ public class BookingController {
     public List<Booking> getAllBookings(@PathVariable Long propertyId) {
         return bookingService.getBookingsByPropertyId(propertyId);
     }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.createBooking(booking);
+    }
+
+    @GetMapping("/tenant/{tenantId}")
+    public List<Booking> getBookingsByTenantId(@PathVariable Long tenantId) {
+        return bookingService.getBookingsByTenantId(tenantId);
+    }
+
+
 }
