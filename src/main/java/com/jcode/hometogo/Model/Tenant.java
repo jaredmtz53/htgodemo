9462a000f7682fa7e9f490a1a3f1f74b6de9a67e
Eduardo <eduardoherrerabarra@gmail.com>
2025-04-21 10:14:57 -0400

    tenant id was not autogenerating, add the documentation to fix

--- src/main/java/com/jcode/hometogo/Model/Tenant.java
+++ src/main/java/com/jcode/hometogo/Model/Tenant.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Entity
 public class Tenant {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String tenantBio;
